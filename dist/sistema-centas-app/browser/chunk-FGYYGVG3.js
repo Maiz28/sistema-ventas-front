1081481589d9126353230c5d0f85894487ec2eb2
Marisol Nuñez <marisol280602@hotmail.com>
2024-08-28 17:11:18 -0600

    Compilado de la app

---
+++ dist/sistema-centas-app/browser/chunk-FGYYGVG3.js
@@ -0,0 +1,1 @@
+var t={API_URL:"http://13.59.53.124:3000/api"};export{t as a};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,3 @@
-export const environment = {};
+export const environment = {
+  API_URL: 'http://13.59.53.124:3000/api',
+};
